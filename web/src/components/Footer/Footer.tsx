95de09b3ebf641ace9c7f98525fb847c3dba74c9
Lisa Yip <imlisayip@gmail.com>
2024-02-07 15:39:48 -0800

    Footer

---
+++ web/src/components/Footer/Footer.tsx
@@ -0,0 +1,24 @@
+const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 z-0 w-full bg-acadia py-[72px] text-center dark:bg-blackPearl">
+      <img
+        src="/images/logo__secret-santa--small.svg"
+        alt="Secret Santa"
+        className="relative mx-auto mb-16"
+      />
+      <p className="font-sans font-medium text-scotchMist">
+        Copyright &copy;2023.{' '}
+        <a
+          href="/"
+          target="_target"
+          rel="noreferrer"
+          className="underline hover:no-underline"
+        >
+          link
+        </a>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
--- web/src/layouts/AuthLayout/AuthLayout.tsx
+++ web/src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,3 +1,5 @@
+import Footer from 'src/components/Footer/Footer'
+
 type AuthLayoutProps = {
   children?: React.ReactNode
 }
@@ -6,14 +8,17 @@ const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
     // By default, the background size has a width of 1440 pixels. By default the background will be wider than the device width, and as tall as it needs to be.
     // Once its hits 1440+ breakpoint, image width of 100 percent and then it can still grow to be as tall as it needs to be.
-    <main className="min-w-screen min-h-screen bg-silverTree bg-auth bg-[length:1440px_auto] bg-[center_top] bg-no-repeat min-[1440px]:bg-[length:100%_auto] dark:bg-nileBlue">
-      {children}
-      <img
-        src="/images/logo__secret-santa.svg"
-        alt="Secret Santa"
-        className="mx-auto mb-24 w-[460px] pt-24"
-      />
-    </main>
+    <>
+      <main className="min-w-screen min-h-screen bg-silverTree bg-auth bg-[length:1440px_auto] bg-[center_top] bg-no-repeat min-[1440px]:bg-[length:100%_auto] dark:bg-nileBlue">
+        {children}
+        <img
+          src="/images/logo__secret-santa.svg"
+          alt="Secret Santa"
+          className="mx-auto mb-24 w-[460px] pt-24"
+        />
+      </main>
+      <Footer />
+    </>
   )
 }
 
